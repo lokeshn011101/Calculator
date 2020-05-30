57ea1cac21679292f0ebbdf0950a3c1e4108b054
lokeshn011101 <lokesh19055@cse.ss.edu.in>
2020-05-31 00:15:43 +0530

    added credits

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
+        <header>Designed and Coded by Lokesh N N</header>
         <Calculator />
       </div>
     );
