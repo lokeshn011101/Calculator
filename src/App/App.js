11467bf5c30daf9a5ef50ba420cf635bfcc94eb8
lokeshn011101 <lokesh19055@cse.ss.edu.in>
2020-05-31 14:03:24 +0530

    added linkedin profile

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,15 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <header>Designed and Coded by Lokesh N N</header>
+        <header>
+          Designed and Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/lokesh-n-n-47a294190/"
+            target="_blank"
+          >
+            Lokesh N N
+          </a>
+        </header>
         <Calculator />
       </div>
     );
